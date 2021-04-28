42035feb5be991bfe935d2d270b0450f227aad20
Daniel Shervheim <danielshervheim@gmail.com>
2021-04-28 12:41:31 -0500

    over to you alex

---
+++ history-demo/add.js
@@ -0,0 +1,13 @@
+const isNumber = require('is-number')
+
+function add(num1, num2) {
+  if (!isNumber(num1) || !isNumber(num2)) {
+    return 404;
+  }
+  
+  return Number(num1) + Number(num2);
+}
+
+module.exports = {
+  add
+}
